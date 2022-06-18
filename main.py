1e334d2aaec7b80a9a686806b263c3422fe15bd6
Bihshtein <alexbihsh@gmail.com>
2022-06-18 16:06:08 +0300

    improvements

--- main.py
+++ main.py
@@ -1,56 +1,45 @@
 import streamlit as st
-import pandas
-import glob
-import json
 import io
+import zipfile
 import os
-import shutil
+from metric_builders import MetricsBuilders
 
 st.set_page_config(layout="wide")
-st.title('GARMIN insights for long distance runners')
-file = st.file_uploader("Please choose a file" )
-if file is not None:
-    bytes_data = file.getvalue()
-    import zipfile
-    local_path= os.path.join(os.getcwd(), 'data')
-    with zipfile.ZipFile(io.BytesIO(bytes_data)) as zip_ref:
-        zip_ref.extractall(local_path)
 
-    dfs = []
-    folders = ['.']
-    all_files = []
-    for folder in folders:
-        path = os.path.join(local_path, 'DI_CONNECT/DI-Connect-User' , 'UDSFile*.json')
 
-        all_files = all_files + glob.glob(path)
-    for file in all_files:
-        dfs.append(pandas.read_json(file))
-    df = pandas.concat(dfs).dropna(subset=['calendarDate'])
+def run_website():
+    st.title('Stride and oxygen insights for long distance runners')
+    file = st.file_uploader("Export all garmin data zip (https://support.garmin.com/en-US/?faq=W1TvTPW8JZ6LfJSfK512Q8)")
+    if file is not None:
+        display_zipped_data(io.BytesIO(file.getvalue()))
 
-    df['vigorousIntensityMinutesRolling'] = df['vigorousIntensityMinutes'].rolling(5).mean()
-    df['moderateIntensityMinutesRolling'] = df['moderateIntensityMinutes'].rolling(5).mean()
-    df = df.assign(date=df['calendarDate'].apply(lambda cell: pandas.to_datetime(cell['date']).date())).sort_values(by='date')
-    health_data_df = df
-    files = []
-    for folder in folders:
-        path = os.path.join(local_path, 'DI_CONNECT/DI-Connect-Fitness' , '*summarizedActivities.json')
-        files = files + glob.glob(path)
-    dfs = []
-    for file in files:
-        _json = json.load(open(file))
-        dfs.append(pandas.DataFrame.from_records(_json[0]['summarizedActivitiesExport']))
-    shutil.rmtree(local_path)
+    if st.button('Display barefoot sample  data'):
+        display_zipped_data('alex_barefoot.zip')
 
-    df = pandas.concat(dfs)
-    df = df[df['name'] == 'Running']
-    df = df[df['avgDoubleCadence']>145]
-    df['avgStrideLengthRolling']=df['avgStrideLength'].rolling(3).mean()
-    df['avgDoubleCadenceRolling']=df['avgDoubleCadence'].rolling(3).mean()
-    df['date'] = pandas.to_datetime(df['beginTimestamp'], unit='ms').dt.date
-    df = df.merge(health_data_df, on='date', how='left')
-    x = df['date']
+    if st.button('Display semi-barefoot sample data'):
+        display_zipped_data('alex_semi_barefoot.zip')
 
-    df = df[['date', 'avgStrideLengthRolling','avgDoubleCadenceRolling']]\
-            .rename(columns={'date':'index'}).set_index('index')
-    st.line_chart(data=df, use_container_width=True)
 
+def display_zipped_data(file):
+    local_folder_path = os.path.join(os.getcwd(), 'data')
+
+    with zipfile.ZipFile(file) as zip_ref:
+        zip_ref.extractall(local_folder_path)
+    user_metrics = MetricsBuilders.build_user_metrics(local_folder_path)
+    running_metrics = MetricsBuilders.build_running_metrics(local_folder_path)
+    metrics_to_display = \
+        ['date', 'restingHeartRate', 'vO2MaxValue', 'avgStrideLengthRolling', 'avgDoubleCadenceRolling']
+    display_metrics = running_metrics.merge(user_metrics, on='date', how='left')[metrics_to_display] \
+        .rename(columns={'date': 'index',
+                         'restingHeartRate' : 'R_HR',
+                         'vO2MaxValue' : 'V02Max',
+                         'avgStrideLengthRolling' : 'Stride',
+                         'avgDoubleCadenceRolling' : 'Cadence'
+                         })\
+        .set_index('index') \
+        .dropna()
+    st.line_chart(display_metrics)
+
+
+if __name__ == '__main__':
+    run_website()
---
+++ metric_builders.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import glob
+import json
+import os
+import shutil
+
+
+class MetricsBuilders:
+
+    @staticmethod
+    def build_user_metrics(local_folder_path):
+        path = os.path.join(local_folder_path, 'DI_CONNECT/DI-Connect-User', 'UDSFile*.json')
+        files = glob.glob(path)
+        dfs = [pd.read_json(file) for file in files]
+        return pd.concat(dfs).dropna(subset=['calendarDate']) \
+            .assign(vigorousIntensityMinutesRolling=lambda d: d['vigorousIntensityMinutes'].rolling(5).mean(),
+                    moderateIntensityMinutesRolling=lambda d: d['moderateIntensityMinutes'].rolling(5).mean(),
+                    date=lambda d: d['calendarDate'].apply(lambda cell: pd.to_datetime(cell['date']).date()))\
+            .sort_values(by='date')
+
+    @staticmethod
+    def build_running_metrics(local_folder_path):
+        path = os.path.join(local_folder_path, 'DI_CONNECT/DI-Connect-Fitness', '*summarizedActivities.json')
+        files = glob.glob(path)
+        dfs = [pd.DataFrame.from_records(json.load(open(file))[0]['summarizedActivitiesExport']) for file in files]
+        shutil.rmtree(local_folder_path)
+
+        return pd.concat(dfs)\
+            .query("name == 'Running' & avgDoubleCadence > 145")\
+            .assign(avgStrideLengthRolling=lambda d: d['avgStrideLength'].rolling(3).mean(),
+                    avgDoubleCadenceRolling=lambda d: d['avgDoubleCadence'].rolling(3).mean(),
+                    date=lambda d: pd.to_datetime(d['beginTimestamp'], unit='ms').dt.date)
