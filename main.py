06cef8286a100d4639a13fbbf88adf9ba4c2eb49
Bihshtein <alexbihsh@gmail.com>
2022-06-24 15:39:36 +0300

    update

--- main.py
+++ main.py
@@ -8,16 +8,25 @@ st.set_page_config(layout="wide")
 
 
 def run_website():
-    st.title('Stride and oxygen insights for long distance runners')
+    st.header('Stride and oxygen insights for long distance runners')
     file = st.file_uploader("Export all garmin data zip (https://support.garmin.com/en-US/?faq=W1TvTPW8JZ6LfJSfK512Q8)")
     if file is not None:
         display_zipped_data(io.BytesIO(file.getvalue()))
+    barefoot = 'Barefoot sample  data'
+    semi_barefoot = 'Semi-barefoot sample data'
+    shoes = 'Shoes sample data'
+    option = st.selectbox(
+        'Pick a sample data option for an impression on the insights',
+        (barefoot, semi_barefoot, shoes))
 
-    if st.button('Display barefoot sample  data'):
+    if option == barefoot:
         display_zipped_data('alex_barefoot.zip')
 
-    if st.button('Display semi-barefoot sample data'):
+    elif option == semi_barefoot:
         display_zipped_data('alex_semi_barefoot.zip')
+
+    elif option == shoes:
+        display_zipped_data('shoes.zip')
 
 
 def display_zipped_data(file):
@@ -46,56 +55,59 @@ def display_zipped_data(file):
         .assign(
             month_year=lambda d: d.date.astype('datetime64[ns]').dt.month.astype(str) + '-' +
                                  d.date.astype('datetime64[ns]').dt.year.astype(str),
-            monthly_average_distance=lambda d: d.groupby(by=d.month_year)['distance'].transform('mean')/100/100,
-            monthly_average_stride= lambda d: d.groupby(by=d.month_year)['avgStrideLength'].transform('mean'),
-            monthly_average_cadence = lambda d: d.groupby(by=d.month_year)['avgDoubleCadence'].transform('mean'),
-            monthly_average_vo2max = lambda d: d.groupby(by=d.month_year)['vO2MaxValue'].transform('mean'),
-            monthly_average_resting_heart_rate = lambda d: d.groupby(by=d.month_year)['restingHeartRate'].transform('mean')) \
+            distance=lambda d: d.groupby(by=d.month_year)['distance'].transform('mean')/100/100,
+            stride=lambda d: d.groupby(by=d.month_year)['avgStrideLength'].transform('mean'),
+            cadence=lambda d: d.groupby(by=d.month_year)['avgDoubleCadence'].transform('mean'),
+            vo2max=lambda d: d.groupby(by=d.month_year)['vO2MaxValue'].transform('mean'),
+            R_HR=lambda d: d.groupby(by=d.month_year)['restingHeartRate'].transform('mean')) \
             .drop_duplicates(subset=['month_year'])\
         .set_index('month_year') \
         .sort_values(by='date')\
-        [['monthly_average_distance', 'monthly_average_stride', 'monthly_average_cadence',
-          'monthly_average_vo2max', 'monthly_average_resting_heart_rate']].\
+        [['distance', 'stride', 'cadence', 'vo2max', 'R_HR']].\
         fillna(0).astype(int)
 
     st.subheader('Monthly summarized stats')
 
-    st.table(summarized)
+    st.dataframe(summarized)
 
     st.subheader('Insights')
 
-    st.caption('1. Cadence')
-    if len(summarized[summarized['monthly_average_cadence'] >= 170].index) == len(summarized.index):
+    st.caption('Cadence')
+    if len(summarized[summarized['cadence'] >= 170].index) == len(summarized.index):
         st.text('Brilliant! You got top cadence, keep it up!')
         cadence_score = 1
-    elif len(summarized[summarized['monthly_average_cadence'] >= 160].index) == len(summarized.index):
-        st.text('Your cadence can and should be improved,'
-                ' try taking smaller steps or taking off your shoes and go above 170 steps per minute')
+    elif len(summarized[summarized['cadence'] >= 160].index) == len(summarized.index):
+        st.text('Your cadence can and should be improved')
+        st.text('Try taking smaller steps or taking off your shoes')
+        st.text('Strive to go above 170 steps per minute')
         cadence_score = 2
     else:
-        st.text('Your cadence has significant room for improvement, '
-                'try to taking smaller steps and go above 160 steps per minute')
+        st.text('Your cadence has significant room for improvement')
+        st.text('Try  taking smaller steps and go above 160 steps per minute')
         cadence_score = 3
 
     st.caption('Stride Length')
     if cadence_score == 1:
-        st.text('It is time to work on your stride, '
-                'try more interval training but do not go overboard! '
-                'More than a few centimeters per month can get you injured!')
-    elif len(summarized[summarized['monthly_average_stride'] >= 100].index) == len(summarized.index):
+        st.text('It is time to work on your stride')
+        st.text('Try more interval training but do not go overboard!')
+        st.text('More than a few centimeters per month can cause injury!')
+    elif len(summarized[summarized['stride'] >= 100].index) == len(summarized.index):
         st.text('Your stride is too long! First make sure you can perform at 170 and above cadence!')
     else:
-        st.text('Focus on your cadence, keep decreasing your stride if needed')
+        st.text('Focus on your cadence')
+        st.text('Keep decreasing your stride if needed')
     st.caption('Resting Heart Rate')
-    if len(summarized[summarized['monthly_average_resting_heart_rate'] <= 70].index) == len(summarized.index):
+    if len(summarized[summarized['R_HR'] <= 70].index) == len(summarized.index):
         st.text('You are in a great aerobic shape, keep it up!')
         aerobic_score = 1
     else:
-        st.text('Try increasing your monthly distance to help further improve your resting heart rate')
+        st.text('Try increasing your monthly distance')
+        st.text('This will decrease your resting heart rate')
         aerobic_score = 2
     st.caption('Vo2Max')
-    if aerobic_score == 1 and len(summarized[summarized['monthly_average_vo2max'] <= 50].index) > 0:
-        st.text('To improve Vo2Max you might have to improve the technique or lower your weight even if it is muscle')
+    if aerobic_score == 1 and len(summarized[summarized['vo2max'] <= 50].index) > 0:
+        st.text('To improve Vo2Max you might have to improve the technique')
+        st.text('Alternatively, reduce your weight, even if it is muscle')
     else:
         st.text('With lower heart rate and better technique Vo2Max will increase')
 
